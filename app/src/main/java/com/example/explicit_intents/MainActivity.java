495972f2ae24e766a44fe6328745d8d8422b824e
Saviour <1907saviour@gmail.com>
2022-04-11 18:03:55 +0530

    pushing to git

---
+++ app/src/main/java/com/example/explicit_intents/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.explicit_intents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText ed_username;
+    private Button login;
+    private EditText ed_password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            ed_username=findViewById(R.id.username);
+            btn
+    }
+
+}
